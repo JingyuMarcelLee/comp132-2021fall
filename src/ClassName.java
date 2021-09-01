88bde86c0cb83c5e03b540596b9792afd29bc1ba
skalakm <skalakm@dickinson.edu>
2021-09-01 10:43:23 -0400

    Added ClassName and ClasseName2

---
+++ src/ClassName.java
@@ -0,0 +1,18 @@
+
+public class ClassName {
+
+	
+	public static void main(String[] args) {
+		ClassName classNameObject = new ClassName();
+		ClassName classNameInstance = new ClassName();
+		// ClassName is a data type already
+		
+		classNameObject.method(4,5);
+		//
+		//classNameObject.method("aoeu4",5);
+	}
+	
+	public void method(int parameter0, int parameter1) {
+		System.out.println("concatenate " + " this string");
+	}
+}
---
+++ src/ClassName2.java
@@ -0,0 +1,18 @@
+
+public class ClassName2 {
+
+	
+	public static void main(String[] args) {
+		ClassName2 classNameObject = new ClassName2();
+		ClassName2 classNameInstance = new ClassName2();
+		// ClassName is a data type already
+		
+		classNameObject.method(4,5);
+		//
+		//classNameObject.method("aoeu4",5);
+	}
+	
+	public void method(int parameter0, int parameter1) {
+		System.out.println("concatenate " + " this string");
+	}
+}
