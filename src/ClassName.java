c8f4cb2ee9edded0299342a3f6c1c81890d3e388
skalakm <skalakm@dickinson.edu>
2021-09-03 11:19:55 -0400

    added account2

--- src/ClassName.java
+++ src/ClassName.java
@@ -1,15 +1,60 @@
 
 public class ClassName {
 
+	private int myIntField;
+	private String myStringField;
+	
+	public ClassName( int initialField) {
+		myIntField = initialField;
+		myStringField = "didnt initialize string field";
+		
+		// dont do this
+		//initialField = myIntField;
+	}
+	
+	public ClassName( int initialIntValue, String initialStringValue) {
+		myIntField = initialIntValue;
+		myStringField = initialStringValue;
+	}
+	
+	public void print() {
+		System.out.println(myIntField + " " + myStringField);
+	}
+	
+	public void add5ToIntField() {
+		myIntField = myIntField +5;
+		print();
+	}
+	
+	public int getMyIntField() {
+		return myIntField;
+	}
+	
+	
 	
 	public static void main(String[] args) {
-		ClassName classNameObject = new ClassName();
-		ClassName classNameInstance = new ClassName();
+		ClassName classNameObject = new ClassName(5);
+		ClassName classNameInstance = new ClassName(3, "aoeu");
+		classNameObject.print();
+		classNameObject.add5ToIntField();
+		classNameInstance.print();
 		// ClassName is a data type already
 		
 		classNameObject.method(4,5);
 		//
 		//classNameObject.method("aoeu4",5);
+		
+		System.out.println( -17/4);
+		System.out.println( 45/80);
+		
+		// mod operator, remainder operator 
+		System.out.println( 17%4);// 1
+		
+		double x = 3.4;
+		System.out.println(.1+.2);
+		
+		boolean b = (4==5); 
+		boolean c = true;
 	}
 	
 	public void method(int parameter0, int parameter1) {
